0181d3579ac476705ced1e0bc86b09e288d633df
JDRL555 <joshuleal3@gmail.com>
2025-02-09 12:46:30 -0400

    feat: created the button component and added the secondary color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import Navbar from './components/Navbar'
+import Button from './components/Button'
 
 export default function App() {
   return (
     <main>
       <Navbar />
+      <main className='flex justify-center mt-10'>
+        <Button background='primary'>
+          Sube tus sospechosos
+        </Button>
+      </main>
     </main>
   )
 }
