14dc42f3a8901166e215b69cc6d3fceb360e8efd
Gabriel <ggonzalezalzualde@gmail.com>
2025-03-07 12:26:40 -0400

    The model is connected with the backend. But the model comparate the imgs saved and not comparate the img pass by the user

--- backend/main.py
+++ backend/main.py
@@ -16,15 +16,15 @@ app = FastAPI()
     a través de la clase Middleware
 '''
 
-origin = {
-    "http://localhost:3000"
-}
+# origin = {
+#     "http://localhost:3000"
+# }
 
-Middleware(
-    origin = origin,
-    allow_headers = ["*"],
-    allow_credentials = True
-)
+# Middleware(
+#     origin = origin,
+#     allow_headers = ["*"],
+#     allow_credentials = True
+# )
 
 
 # Initialize the model
@@ -34,20 +34,40 @@ oauth2 = OAuth2PasswordBearer(tokenUrl = "comparate")
 UPLOAD_FOLDER = 'api/images'
 os.makedirs(UPLOAD_FOLDER, exist_ok = True)
 
-async def exist_images() -> bool:
+# async def exist_images() -> bool:
+#     os.listdir()
 
-    os.listdir()
+
+'''
+    FUNCTIONS WE'LL USE TO WORK IN THE COMPARATION
+'''
+
+async def validate_image(image: UploadFile):
+
+    if not image.filename.lower().endswith(('.jpg', '.png', 'jpeg')):
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,
+                            detail = "Image format don't accept")
+    file = await image.read()
+    return file
+
+'''
+    ENDPOINTS WHERE WE'LL WORK THE CONNECTION FRONT AND BACK
+'''
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hola World"}
 
-@app.get("/ping")
-def ping():
-    return {"message": "pong"}
+@app.get("/uploaded-faces")
+async def get_images():
 
+    if not os.listdir("api/images/"):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = "There is not images")
+    return {"path_images" : os.listdir("api/images/")}
+    
 
-@app.post("/upload-images")
+@app.post("/upload-faces")
 async def upload_image(images: List[UploadFile] = File(...)):
     try:
         # Clear existing images
@@ -65,68 +85,48 @@ async def upload_image(images: List[UploadFile] = File(...)):
                 image_file.write(image_data)
             list_path.append(file_path)
 
+        face_model.add_known_face(list_path)
+
         return JSONResponse(
-            content={"message": "Images uploaded successfully"}, status_code=200
+            content={"message": "Images uploaded successfully", "path" : list_path}, status_code = 200
         )
     except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        return JSONResponse(content={"error": str(e)}, status_code = 500)
 
-# Comparate criminals with others suspects
-# @app.post("/compare-face")
-# async def upload_criminal(image: Annotated[UploadFile, "Image of the suspect"]):
-MAX_SIZE = 10 * 1024 * 1024
 @app.post("/compare-faces")
-async def compare_faces(file: UploadFile = File(...)):
+async def compare_faces(image: UploadFile = File(...)):
 
-    """
-    Este endpoint recibe una imagen y la compara contra las caras conocidas 
-    almacenadas en face_model.known_face_embeddings.
-    """
-    # 1. Validar que sea una imagen
-    if not file.filename.lower().endswith((".png", ".jpg", ".jpeg")):
-        raise HTTPException(status_code=400, detail="Formato de imagen no soportado.")
+    # Validar imagen
+    if not image.filename.lower().endswith(('.jpg', '.png', 'jpeg')):
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,
+                            detail = "Image format don't accept")
+    image_bytes = await image.read()
+
+    # Conocer el resultado 
+
+    try: 
+        image_pil = Image.open(BytesIO(image_bytes))
+    except Exception as e: 
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,
+                            detail = f"Err to open the image {e}")
     
-    # 2. Guardar temporalmente el archivo o leerlo en memoria
-    # Opción A: Leerlo directamente en memoria
-    file_bytes = await file.read()
-    if len(file_bytes) > MAX_SIZE:
-        raise HTTPException(status_code = 400, detail = "El archivo excede de los parámetros")
-    # 3. Crear un objeto PIL.Image para pasarlo al modelo
+
+    # Capturar las caras y comparar las imagenes 
     try:
-        image = Image.open(BytesIO(file_bytes))
+        matches, matched_boxes = face_model.compare_face_with_known_faces(image_pil)
+        response_data = []
+        for i, (path, distance) in enumerate(matches):
+            response_data.append({
+                "known_face_path": path,
+                "distance": distance,
+                "box": matched_boxes[i].tolist()
+            })
+        if not response_data:
+            return {"message": "No se encontraron coincidencias", "information" : response_data}
+        return {"message": "Se encontraron coincidencias", "results": response_data}
     except Exception as e:
-        raise HTTPException(status_code = 400, detail = f"Error al abrir la imagen: {e}")
-    
+        raise HTTPException(status_code=500, detail=str(e))
 
-
-    
-    # 4. Llamar al método compare_face_with_known_faces del modelo
-    matches, matched_boxes = face_model.compare_face_with_known_faces(image)
-    
-    list_images = []
-    
-    for path_image in os.listdir(UPLOAD_FOLDER):
-        with open(path_image, "rb") as path_img:
-            list_images.append(path_img)
-    # matches es una lista de tuplas (path, distance)
-    # matched_boxes es la lista de bounding boxes correspondientes
-    # 5. Procesar la respuesta para devolverla al frontend
-    response_data = []
-    for i, (path, distance) in enumerate(matches):
-
-        response_data.append({
-            "known_face_path": path,
-            "distance": distance,
-            "box": matched_boxes[i].tolist()  # Convertir a lista para JSON
-        })
-    
-    if not response_data:
-        return f"Array final: {response_data}"
-    raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail = "Hay un error extraño")
-    # if not response_data:
-    #     return {"message": "No se encontraron coincidencias"}
-    
-    # return {
-    #     "message": "Se encontraron coincidencias",
-    #     "results": response_data
-    # }
+@app.get("/testing")
+async def test():
+    return {"test" : "testeasndo"}
